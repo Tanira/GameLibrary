20954389e69742f25a55af6a8adbf7327feb42bb
Eric Hill <eric.a.hill@hotmail.com>
2016-08-31 21:58:58 -0400

    initial commit

---
+++ GameLibrary/Views/Shared/StatusBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GameLibrary.Views.Shared
+{
+    /// <summary>
+    /// Interaction logic for StatusBar.xaml
+    /// </summary>
+    public partial class StatusBar : UserControl
+    {
+        public StatusBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
